4a19f7474c09f1644f3183fab8828f1ff7f8a96f
Aidas Aleksiejus <aia@danskebank.lt>
2018-03-02 15:47:19 +0200

    EOW

---
+++ Testing/ConsoleApp/TestOnLinux/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestOnLinux
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
