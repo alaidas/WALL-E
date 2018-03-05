01bcd32aef93e502dce12f7239738df61bc165a2
Aidas Aleksiejus <aia@danskebank.lt>
2018-03-05 16:15:19 +0200

    Added docker support.

--- Testing/ConsoleApp/TestOnLinux/Program.cs
+++ Testing/ConsoleApp/TestOnLinux/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace TestOnLinux
 {
@@ -6,7 +7,25 @@ namespace TestOnLinux
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MainAsync(args).Wait();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            Console.WriteLine("Hello async World!");
+
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine(DateTime.UtcNow.ToString());
+                    await Task.Delay(1000);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
         }
     }
 }
